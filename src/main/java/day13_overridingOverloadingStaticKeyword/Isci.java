cf43668879d08865076960193da946d6458ebfb8
Nihal Bakisgan <nihalkaratasbakisgan@gmail.com>
2022-11-03 10:47:49 +0100

    sildim

--- src/main/java/day13_overridingOverloadingStaticKeyword/Isci.java
+++ src/main/java/day13_overridingOverloadingStaticKeyword/Isci.java
@@ -6,11 +6,6 @@ public class Isci {
         System.out.println("isciler gunde 8 saat calisir");
     }
 
-    public static void main(String[] args) {
-        toplama(3,5);
-        
-    }
 
-    private static void toplama(int i, int i1) {
-    }
+
 }
